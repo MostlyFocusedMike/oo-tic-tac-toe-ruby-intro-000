1595932295aba11bdc9066c22e20e68c2ee117c2
Mike C <mikecronin92@gmail.com>
2018-04-05 04:51:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,7 +12,7 @@ class TicTacToe
     self.gets_input
     self.input_to_index
     if self.valid_move?
-      char = current_player(board) 
+      self.current_player(board) 
       move(board,index,char) 
       display_board
     else 
