41526b1430a81361e7aa7c8ad9ca4faf74dd9ef8
Mike C <mikecronin92@gmail.com>
2018-04-05 05:14:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,14 +3,14 @@ class TicTacToe
   [0,1,2], [3,4,5],[6,7,8],[0,3,6],
   [1,4,7],[2,5,8],[0,4,8],[2,4,6]
   ]
-  def initialize
-    @b = Array.new(9, " ")
+  def initialize(board = nil) 
+    @board = board || Array.new(9, " ")
   end
   
   # GENERAL METHODS ############
   
   def display_board
-    @board [0,3,6].map {|i| " #{@b[i]} | #{@b[i+1]} | #{@b[i+2]} \n"}.join("#{'-'*11}\n")
+    puts [0,3,6].map {|i| " #{@b[i]} | #{@b[i+1]} | #{@b[i+2]} \n"}.join("#{'-'*11}\n")
   end
   
   # MAIN FUNCTION METHODS #########
